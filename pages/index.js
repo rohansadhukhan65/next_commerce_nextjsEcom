65e0d2fa146e748463907165c1b3e067f1908eb6
rohansadhukhan65 <sadhukhanrohan15@gmail.com>
2022-04-23 21:46:41 +0530

    DESKTOP-14HOD9D => Sat, Apr 23, 2022  9:46:41 PM

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-
+import React from "react";
 // Import Swiper React components
+import SwiperCore, { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+SwiperCore.use([Navigation]);
 
 // Import Swiper styles
 import 'swiper/css';
@@ -20,6 +21,9 @@ import BannerTwo from '../public/images/BannerTwo.jpg';
 import BannerThree from '../public/images/BannerThree.jpg';
 
 export default function Home() {
+  const navigationPrevRef = React.useRef();
+  const navigationNextRef = React.useRef();
+  
   return (
     <div  >
       
@@ -50,21 +54,71 @@ export default function Home() {
 
 
       {/* Featured Product Section ..... */}
-      <div className="container my-5">
-        <h1 className='text-center fw-light'>Featured Product</h1>
+      <div className="container my-5 p-5">
+        <h1 className='text-center my-3 fw-light'>Featured Product</h1>
         <Swiper
-          spaceBetween={50}
+          spaceBetween={20}
           slidesPerView={3}
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+                   
         >
           <SwiperSlide>
-            <div className="card" style={{ width: '23rem' }}></div>
+            <div className="card shadow" style={{ width: '100%' }}>
+              <Image  src={bannerOne} className="card-img-top" alt="Picture of the author" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary mx-auto">Go somewhere</a>
+              </div>
+            </div>
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          ...
+
+          <SwiperSlide>
+            <div className="card shadow" style={{ width: '100%' }}>
+              <Image src={bannerOne} className="card-img-top" alt="Picture of the author" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary mx-auto">Go somewhere</a>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card shadow" style={{ width: '100%' }}>
+              <Image src={bannerOne} className="card-img-top" alt="Picture of the author" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary mx-auto">Go somewhere</a>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card shadow" style={{ width: '100%' }}>
+              <Image src={bannerOne} className="card-img-top" alt="Picture of the author" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary mx-auto">Go somewhere</a>
+              </div>
+            </div>
+          </SwiperSlide>
+ 
+ 
+            {/* Slider Controlllers ..... */}
+          <div className="swiper-button-prev" />
+          <div className="swiper-button-next" />
+          <div ref="{navigationPrevRef}"/>
+            <div ref="{navigationNextRef}"/>
+           
+
         </Swiper>
       </div>
 
