ae7ef5cd07d76fb25410ddd2ec71460aac9dce2a
Sanchari2506 <sancharirakshit2709@gmail.com>
2022-04-23 19:26:24 +0530

    DESKTOP-1C614D2 => Sat, Apr 23, 2022  7:26:24 PM

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ const header = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">NextCommerce</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
